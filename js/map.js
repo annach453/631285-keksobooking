e07feeb9dcc6ab0581ed60fcfc405ae239e6132d
Anna <anna.ch453@gmail.com>
2018-04-14 21:22:29 +0300

    исправляет переменные и параметры

--- js/map.js
+++ js/map.js
@@ -13,10 +13,10 @@ var getRandomArrayValue = function (array) {
 // Перетасовывает значения массива в случайном порядке
 var shuffleArray = function (source) {
   var array = source.slice();
-  return array.sort(function (a, b) {
+  return array.sort(function () {
     return Math.random() - 0.5;
   });
-}
+};
 
 // Возвращает случайное количество значений массива
 var getRandomArrayValues = function (source) {
@@ -62,38 +62,38 @@ var testDataSource = {
     'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
     'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
   ],
-}
+};
 var markQuantity = 8; // Количество меток на карте
 var markFragmentTemplate = document.querySelector('template').content.querySelector('.map__pin'); // Фрагмент для вывода меток
 var adFragmentTemplate = document.querySelector('template').content.querySelector('.map__card'); // Фрагмент для вывода объявления
 var markSize = { /* Размер метки */
   width: 50,
   height: 70
-}
+};
 
 // Генерирует массив тестовых данных
-var renderTestData = function (testDataSource, quantity) {
+var renderTestData = function (testData, quantity) {
   var testdata = [];
-  var titles = shuffleArray(testDataSource.titleSrc);
+  var titles = shuffleArray(testData.titleSrc);
   for (var i = 0; i < quantity; i++) {
     var locationX = getRandomElement(300, 900);
     var locationY = getRandomElement(150, 500);
     testdata.push({
       author: {
-        avatar: testDataSource.avatarSrc.replace('{{xx}}', new Intl.NumberFormat('ru-RU', { minimumIntegerDigits: 2, useGrouping: false }).format(i + 1)),
+        avatar: testData.avatarSrc.replace('{{xx}}', new Intl.NumberFormat('ru-RU', {minimumIntegerDigits: 2, useGrouping: false}).format(i + 1)),
       },
       offer: {
         title: titles[i],
         address: locationX + ', ' + locationY,
         price: getRandomElement(1000, 1000000),
-        type: getRandomArrayValue(testDataSource.typeSrc),
+        type: getRandomArrayValue(testData.typeSrc),
         rooms: getRandomElement(1, 5),
         guests: getRandomElement(1, 10),
-        checkin: getRandomArrayValue(testDataSource.checkinChecoutTimeSrc),
-        checkout: getRandomArrayValue(testDataSource.checkinChecoutTimeSrc),
-        features: getRandomArrayValues(testDataSource.featuresSrc),
+        checkin: getRandomArrayValue(testData.checkinChecoutTimeSrc),
+        checkout: getRandomArrayValue(testData.checkinChecoutTimeSrc),
+        features: getRandomArrayValues(testData.featuresSrc),
         description: '',
-        photos: shuffleArray(testDataSource.photosSrc)
+        photos: shuffleArray(testData.photosSrc)
       },
       location: {
         x: locationX,
@@ -105,35 +105,35 @@ var renderTestData = function (testDataSource, quantity) {
 };
 
 // Создает фрагмент c 1 меткой
-var createMarkFragment = function (dataSourceItem, markFragmentTemplate, markSize) {
-  var fragment = markFragmentTemplate.cloneNode(true);
-  fragment.style.left = dataSourceItem.location.x - markSize.width / 2 + 'px';
-  fragment.style.top = dataSourceItem.location.y - markSize.height + 'px';
+var createMarkFragment = function (dataSourceItem, markFragmentTemplateSrc, markSizeSrc) {
+  var fragment = markFragmentTemplateSrc.cloneNode(true);
+  fragment.style.left = dataSourceItem.location.x - markSizeSrc.width / 2 + 'px';
+  fragment.style.top = dataSourceItem.location.y - markSizeSrc.height + 'px';
   fragment.querySelector('img').src = dataSourceItem.author.avatar;
   fragment.querySelector('img').alt = dataSourceItem.offer.title;
   return fragment;
 };
 
-// Генериркет список меток
-var createMarkList = function (dataSource, markFragmentTemplate, markSize) {
+// Генерирует список меток
+var createMarkList = function (dataSource, markFragmentTemplateSrc, markSizeSrc) {
   var itemList = document.createDocumentFragment();
   for (var i = 0; i < dataSource.length; i++) {
-    itemList.appendChild(createMarkFragment(dataSource[i], markFragmentTemplate, markSize));
+    itemList.appendChild(createMarkFragment(dataSource[i], markFragmentTemplateSrc, markSizeSrc));
   }
   return itemList;
-}
+};
 
 // Добавляет метки на карту
-var addMarkListToMap = function (dataSource, markFragmentTemplate, markSize) {
+var addMarkListToMap = function (dataSource, markFragmentTemplateSrc, markSizeSrc) {
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
-  var markList = createMarkList(dataSource, markFragmentTemplate, markSize);
+  var markList = createMarkList(dataSource, markFragmentTemplateSrc, markSizeSrc);
   map.querySelector('.map__pins').appendChild(markList);
-}
+};
 
 // Создает фрагмент с объявлением
-var createAdFragment = function (dataSourceItem, adFragmentTemplate) {
-  var fragment = adFragmentTemplate.cloneNode(true);
+var createAdFragment = function (dataSourceItem, markFragmentTemplateSrc) {
+  var fragment = markFragmentTemplateSrc.cloneNode(true);
 
   fragment.querySelector('.popup__title').textContent = dataSourceItem.offer.title;
   fragment.querySelector('.popup__text--address').textContent = dataSourceItem.offer.address;
@@ -151,43 +151,34 @@ var createAdFragment = function (dataSourceItem, adFragmentTemplate) {
     case 'palace':
       fragment.querySelector('.popup__type').textContent = 'Дворец';
       break;
-  };
-  fragment.querySelector('.popup__text--capacity').textContent = dataSourceItem.offer.rooms + ' комнаты для '+ dataSourceItem.offer.guests +' гостей';
+  }
+  fragment.querySelector('.popup__text--capacity').textContent = dataSourceItem.offer.rooms + ' комнаты для ' + dataSourceItem.offer.guests + ' гостей';
   fragment.querySelector('.popup__text--time').textContent = 'Заезд после ' + dataSourceItem.offer.checkin + ', выезд до ' + dataSourceItem.offer.checkout;
 
   // Добавляет доступные удобства
   fragment.querySelector('.popup__features').textContent = '';
   for (var i = 0; i < dataSourceItem.offer.features.length; i++) {
-    fragment.querySelector('.popup__features')
-      .appendChild(document.createElement('li'))
-      .classList.add('popup__feature', 'popup__feature--' + dataSourceItem.offer.features[i]);
-  };
+    fragment.querySelector('.popup__features').appendChild(document.createElement('li')).classList.add('popup__feature', 'popup__feature--' + dataSourceItem.offer.features[i]);
+  }
 
   fragment.querySelector('.popup__description').textContent = dataSourceItem.offer.description;
 
   // Добавляет фото
   var imageFragment = fragment.querySelector('.popup__photo');
   fragment.querySelector('.popup__photos').textContent = '';
-  for (var i = 0; i < dataSourceItem.offer.photos.length; i++) {
-    fragment.querySelector('.popup__photos')
-      .appendChild(imageFragment.cloneNode()).src = dataSourceItem.offer.photos[i];
-  };
+  for (i = 0; i < dataSourceItem.offer.photos.length; i++) {
+    fragment.querySelector('.popup__photos').appendChild(imageFragment.cloneNode()).src = dataSourceItem.offer.photos[i];
+  }
 
   return fragment;
-}
+};
 
-// Генерирует 1 объявление
-var createAd = function (dataSourceItem, adFragmentTemplate) {
-  var ad = document.createDocumentFragment();
-  return ad.appendChild(createAdFragment(dataSourceItem, adFragmentTemplate));
-}
-
-// Добавляет объявление на карту
-var addAdToMap = function (dataSourceItem, adFragmentTemplate) {
+// Генерирует и добавляет объявление на карту
+var addAdToMap = function (dataSourceItem, adFragmentTemplateSrc) {
   var map = document.querySelector('.map');
-  var ad = createAd(dataSourceItem, adFragmentTemplate);
+  var ad = document.createDocumentFragment().appendChild(createAdFragment(dataSourceItem, adFragmentTemplateSrc));
   map.insertBefore(ad, map.querySelector('.map__filters-container'));
-}
+};
 
 var testData = renderTestData(testDataSource, markQuantity);
 addMarkListToMap(testData, markFragmentTemplate, markSize);
